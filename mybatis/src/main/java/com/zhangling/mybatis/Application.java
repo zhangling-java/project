79086bc3ff7aa1979b6b83ce8b66b2a5a63c17c4
steven <steven.zhang@ccydsz.com>
2018-05-24 17:19:33 +0800

    配置  fastjson

--- mybatis/src/main/java/com/zhangling/mybatis/Application.java
+++ mybatis/src/main/java/com/zhangling/mybatis/Application.java
@@ -1,10 +1,20 @@
 package com.zhangling.mybatis;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan
@@ -16,5 +26,22 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
+    }
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        //1、先定义一个convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter=new FastJsonHttpMessageConverter();
+        //2、添加fastjson的配置信息，比如是否要格式化返回的json数据；
+        FastJsonConfig fastJsonConfig=new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //附加：处理中文乱码
+        List<MediaType> fastMedisTypes = new ArrayList<MediaType>();
+        fastMedisTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMedisTypes);
+        //3、在convert中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter=fastConverter;
+        return new HttpMessageConverters(converter);
     }
 }
---
+++ spring-boot/src/main/java/com/zhangling/spring/boot/config/ApplicationConfig.java
@@ -0,0 +1,29 @@
+package com.zhangling.spring.boot.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        // 处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        converters.add(fastConverter);
+    }
+}
